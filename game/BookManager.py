df44c9cc22bebf52db4ee61e974f52ffea71abc2
miura kento <miken@miken.local>
2018-03-24 21:14:31 +0900

    create standart

---
+++ game/BookManager.py
@@ -0,0 +1,22 @@
+class Node:
+    child = Node()
+    sibling = Node()
+    point_x = None
+    point_y = None
+
+
+class BookManager:
+	def __init__(self):
+		Root = None
+
+
+	def find(self, __board):
+		node = Root
+		history = ##
+
+		__board.listing_puttable(1)
+		if(!history): return __board.get_puttable_list()
+
+
+
+
